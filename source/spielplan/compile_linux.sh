2463f05c64a568e4126105544830b4eb1e864c10
Robin Meis <robin.meis@smartnoob.de>
2015-09-06 01:57:20 +0200

    Compile

---
+++ source/spielplan/compile_linux.sh
@@ -0,0 +1,4 @@
+rm spielplan_eingeben-l32 spielplan_eingeben-l64
+
+#gcc -m32 plan_eingabe.c -o "spielplan_eingeben_l32"
+gcc -m64 plan_eingabe.c -o "spielplan_eingeben-l64"
---
+++ source/spielplan/compile_windows.sh
@@ -0,0 +1,4 @@
+rm Spielplan_eingeben-w32.exe Spielplan_eingeben-w64.exe
+
+i686-w64-mingw32-gcc plan_eingabe.c -o Spielplan_eingeben-w32.exe
+x86_64-w64-mingw32-gcc plan_eingabe.c -o Spielplan_eingeben-w64.exe
