7b0515102b31ae57eee7504ad3d2f57da5a88bdf
Robin Meis <robin.meis@smartnoob.de>
2015-09-06 01:57:38 +0200

    Corrected path

--- source/SoccerTool/compile_windows.sh
+++ source/SoccerTool/compile_windows.sh
@@ -1,4 +1,4 @@
 rm SoccerTool-w64.exe
 
-i686-w64-mingw32-gcc source/main.c -o SoccerTool-w32.exe
-x86_64-w64-mingw32-gcc source/main.c -o SoccerTool-w64.exe
+i686-w64-mingw32-gcc main.c -o SoccerTool-w32.exe
+x86_64-w64-mingw32-gcc main.c -o SoccerTool-w64.exe
