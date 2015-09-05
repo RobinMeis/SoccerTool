266bfbba65e72c866d2bafa5df107f7f5594ae5b
Robin Meis <robin.meis@smartnoob.de>
2015-09-05 12:51:16 +0200

    Moved

--- compile_linux.sh
+++
@@ -1,4 +0,0 @@
-rm binaries/SoccerTool_l32 binaries/SoccerTool_l64
-
-#gcc -m32 source/main.c -o binaries/SoccerTool_l32 `pkg-config --libs gtk+-3.0`
-gcc `pkg-config gtk+-2.0 --cflags --libs gtk+-2.0` -m64 source/main.c -o binaries/SoccerTool_64
--- compile_windows.sh
+++
@@ -1,4 +0,0 @@
-rm binaries/SoccerTool-w32.exe binaries/SoccerTool-w64.exe
-
-i686-w64-mingw32-gcc source/main.c -o binaries/SoccerTool-w32.exe
-x86_64-w64-mingw32-gcc source/main.c -o binaries/SoccerTool-w64.exe
